b1477383aefb9d007834de3bed2be5fd91ffb3ef
evgeniykovalsky <evgeniykovalsky04386@gmail.com>
2022-09-09 20:41:55 +0300

    final

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState,useEffect} from "react";
+import RandomOrg from "random-org";
+import Numbers from "./components/Numbers";
+import './style.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+  const [value,setValue]=useState([]);
+  const [minValue,setMin]=useState(0);
+  const [maxValue,setMax]=useState(0);
+  const [amountValue,setAmount]=useState(0);
+  
+
+  useEffect(()=>{
+    let random = new RandomOrg({ apiKey: 'fd0c34e1-c6fc-491d-9154-0fbb2a859f2a' });
+    random.generateIntegers({ min: minValue, max: maxValue, n: amountValue })
+    .then(function(result) {
+          
+   //myNumbers(result.random.data);
+       setValue(result.random.data);
+    });
+},[amountValue]);
+   
+ 
+ 
+
+return (
+  <>
+   <input type="text" placeholder="Enter min" 
+        onChange={event=>setMin(event.target.value)}/>
+         <input type="text" placeholder="Enter max" 
+        onChange={event=>setMax(event.target.value)}
+       
+        />
+         <input type="text" placeholder="Enter amount" 
+        onChange={event=>setAmount(event.target.value)}
+        />
+
+  <h1>Random Numbers</h1>
+  <div className="res">
+  {value.map((item)=><Numbers key={Math.random (). toString (36) .substr (2, 9)} data={item} />)}
+ 
+  </div>
+  </>
+ );
+
 }
+
 
 export default App;
---
+++ src/components/Numbers.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Numbers({data}){
+
+    return(
+        <>
+     
+         <div>{data}</div>
+        </>
+    )
+}
+
